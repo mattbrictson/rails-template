9637d2aa82a11d4fd1f1bf1dbbe1159180b5b957
Matt Brictson <matt@mattbrictson.com>
2015-05-09 16:00:06 -0700

    Improve secure_headers config based on latest ver

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,4 +1,11 @@
-# For more information: https://github.com/twitter/secureheaders
+# See https://github.com/twitter/secureheaders#configuration
 ::SecureHeaders::Configuration.configure do |config|
+  config.hsts = { :max_age => 20.years.to_i, :include_subdomains => false }
+  config.x_frame_options = { :value => "SAMEORIGIN" }
+  config.x_xss_protection = { :value => 1, :mode => "block" }
+  config.x_content_type_options = { :value => "nosniff" }
+  config.x_download_options = false
+  config.x_permitted_cross_domain_policies = { :value => "none" }
   config.csp = false
+  config.hpkp = false
 end
