b3a96b604a87ddadab650d6a04c721d1da071101
Matt Brictson <matt@mattbrictson.com>
2016-01-11 09:51:57 -0800

    Fix secure_headers deprecation warnings

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,11 +1,11 @@
 # See https://github.com/twitter/secureheaders#configuration
-::SecureHeaders::Configuration.configure do |config|
-  config.hsts = { :max_age => 20.years.to_i, :include_subdomains => false }
-  config.x_frame_options = { :value => "SAMEORIGIN" }
-  config.x_xss_protection = { :value => 1, :mode => "block" }
-  config.x_content_type_options = { :value => "nosniff" }
+::SecureHeaders::Configuration.default do |config|
+  config.hsts = "max-age=#{20.years.to_i}"
+  config.x_frame_options = "SAMEORIGIN"
+  config.x_xss_protection = "1; mode=block"
+  config.x_content_type_options = "nosniff"
   config.x_download_options = false
-  config.x_permitted_cross_domain_policies = { :value => "none" }
+  config.x_permitted_cross_domain_policies = "none"
   config.csp = false
   config.hpkp = false
 end
