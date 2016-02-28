bfdceecfb8fbf8354d0a74503071a721a1672c26
Matt Brictson <matt@mattbrictson.com>
2016-02-27 16:01:46 -0800

    Migrate to secure_headers 3.x

--- app/template.rb
+++ app/template.rb
@@ -2,11 +2,6 @@ apply "app/assets/javascripts/application.js.rb"
 copy_file "app/assets/stylesheets/application.css.scss"
 remove_file "app/assets/stylesheets/application.css"
 
-insert_into_file "app/controllers/application_controller.rb",
-                 :after => /protect_from_forgery.*\n/ do
-  "  ensure_security_headers\n"
-end
-
 copy_file "app/controllers/home_controller.rb"
 copy_file "app/helpers/javascript_helper.rb"
 copy_file "app/helpers/layout_helper.rb"
--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,8 +4,8 @@
   config.x_frame_options = "SAMEORIGIN"
   config.x_xss_protection = "1; mode=block"
   config.x_content_type_options = "nosniff"
-  config.x_download_options = false
+  config.x_download_options = SecureHeaders::OPT_OUT
   config.x_permitted_cross_domain_policies = "none"
-  config.csp = false
-  config.hpkp = false
+  config.csp = SecureHeaders::OPT_OUT
+  config.hpkp = SecureHeaders::OPT_OUT
 end
