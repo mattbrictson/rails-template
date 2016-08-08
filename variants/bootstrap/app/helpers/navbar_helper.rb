61947c471381dcdf1ae1c5a957907fb437300542
Matt Brictson <matt@mattbrictson.com>
2016-08-08 16:28:12 -0700

    Fix Rubocop violations

--- variants/bootstrap/app/helpers/navbar_helper.rb
+++ variants/bootstrap/app/helpers/navbar_helper.rb
@@ -13,7 +13,12 @@ module NavbarHelper
   def navbar_link_to(label, path, options={})
     active_when = options.delete(:active_when) { Hash.new }
     active = active_when.all? do |key, value|
-      value === params[key].to_s
+      case value
+      when Regexp
+        params[key].to_s =~ value
+      else
+        params[key].to_s == value
+      end
     end
 
     content_tag(:li, :class => ("active" if active)) do
--- variants/bootstrap/test/helpers/navbar_helper_test.rb
+++ variants/bootstrap/test/helpers/navbar_helper_test.rb
@@ -10,12 +10,15 @@ class NavbarHelperTest < ActionView::TestCase
       "Test",
       "http://example.com",
       :title => "foo",
-      :active_when => { :controller => "users" })
+      :active_when => { :controller => "users" }
+    )
 
     assert_equal(
-      '<li class="active"><a title="foo" href="http://example.com">Test</a></li>',
+      '<li class="active">'\
+      '<a title="foo" href="http://example.com">Test</a>'\
+      "</li>",
       tag
-      )
+    )
   end
 
   test "#navbar_link_to honors regular expressions" do
@@ -24,12 +27,13 @@ class NavbarHelperTest < ActionView::TestCase
     tag = navbar_link_to(
       "Test",
       "http://example.com",
-      :active_when => { :controller => /^user.*/ })
+      :active_when => { :controller => /^user.*/ }
+    )
 
     assert_equal(
       '<li class="active"><a href="http://example.com">Test</a></li>',
       tag
-      )
+    )
   end
 
   test "#navbar_link_to otherwise doesn't add active class" do
