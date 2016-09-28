80c9d3efb8df29d8086bd756c51440c90821fad8
Matt Brictson <matt@mattbrictson.com>
2016-09-28 09:27:10 -0700

    Remove defunct Turbolinks.enableProgressBar()

--- app/assets/javascripts/application.js.rb
+++ app/assets/javascripts/application.js.rb
@@ -1,6 +1,3 @@
 insert_into_file "app/assets/javascripts/application.js",
                  "//= require jquery.turbolinks\n",
                  :before => %r{^//= require turbolinks}
-
-append_to_file "app/assets/javascripts/application.js",
-               "\nTurbolinks.enableProgressBar();\n"
