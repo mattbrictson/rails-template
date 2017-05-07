f163eb1245b1a1881bd1857a80fc26d86419947a
Matt Brictson <matt@mattbrictson.com>
2017-05-07 10:40:03 -0700

    Revert "Add bootscale gem for boot speedup"
    This reverts commit 40caf70c730044cd745e82cb979e249346c43269.

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,6 @@ gsub_file "config/application.rb",
 insert_into_file "config/application.rb", :before => /^  end/ do
   <<-'RUBY'
 
-    # Bootscale needs this to prevent stale cache
-    initializer :regenerate_require_cache, :before => :load_environment_config do
-      Bootscale.regenerate
-    end
-
     # Ensure non-standard paths are eager-loaded in production
     # (these paths are also autoloaded in development mode)
     # config.eager_load_paths += %W(#{config.root}/lib)
