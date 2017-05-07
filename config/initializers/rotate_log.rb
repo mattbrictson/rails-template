775c2d61ae9eb7576dccd0a6d5f07f98863af6fe
Matt Brictson <matt@mattbrictson.com>
2017-05-07 10:39:59 -0700

    Reduce local log file size

--- config/initializers/rotate_log.rb
+++ config/initializers/rotate_log.rb
@@ -4,7 +4,7 @@
 if Rails.env.development? || Rails.env.test?
   log_file = Rails.root.join("log", "#{Rails.env}.log")
 
-  if log_file.file? && log_file.size > 20_000_000
+  if log_file.file? && log_file.size > 5_000_000
     FileUtils.cp(log_file, "#{log_file}.1")
     log_file.truncate(0)
   end
