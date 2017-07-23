54d2def58e5e564a14cd5eef0349659543d7d4dd
Matt Brictson <matt@mattbrictson.com>
2017-07-23 15:49:06 -0700

    Use branch when running template from https URL

--- template.rb
+++ template.rb
@@ -75,6 +75,10 @@ def add_template_repository_to_source_path
       "https://github.com/mattbrictson/rails-template.git",
       tempdir
     ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{rails-template/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git :checkout => branch }
+    end
   else
     source_paths.unshift(File.dirname(__FILE__))
   end
