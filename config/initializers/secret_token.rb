18134ddfc69fbdf31f96fe1f85c0700f6d96d3e4
Matt Brictson <matt@mattbrictson.com>
2017-12-29 15:20:40 -0600

    Compatible with Heroku out of the box

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_key_base = ENV.fetch("RAILS_SECRET_KEY_BASE")
+Rails.application.config.secret_key_base = \
+  ENV.fetch("RAILS_SECRET_KEY_BASE") { ENV.fetch("SECRET_KEY_BASE") }
