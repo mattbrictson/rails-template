a6621d0d475f713fce04b01042fa0e9fab1a3030
Matt Brictson <matt@mattbrictson.com>
2018-02-19 18:52:30 -0800

    Disable bootsnap
    bootstap + dotenv-rails + spring causes Rails to fail to boot :(

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,3 @@
 gsub_file "config/application.rb",
           "# config.time_zone = 'Central Time (US & Canada)'",
           'config.time_zone = "Pacific Time (US & Canada)"'
-
-insert_into_file "config/boot.rb",
-                 %Q(require "bootsnap/setup" unless ENV["DISABLE_BOOTSNAP"]\n),
-                 :after => %r{bundler/setup.*$\n}
