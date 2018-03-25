8dd229c8e7bd9ca50ad786207ec13fed1e65663d
Matt Brictson <matt@mattbrictson.com>
2018-03-24 17:11:38 -0700

    Latest version of mocha

--- test/support/mocha.rb
+++ test/support/mocha.rb
@@ -1,4 +1,4 @@
 # Mocha provides mocking and stubbing helpers
-require "mocha/mini_test"
+require "mocha/minitest"
 Mocha::Configuration.prevent(:stubbing_non_existent_method)
 Mocha::Configuration.warn_when(:stubbing_non_public_method)
