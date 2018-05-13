2197f1d9193b4ef554ec1302b9d2ade859f724e8
Matt Brictson <matt@mattbrictson.com>
2018-05-13 12:19:36 -0700

    Gracefully handle routing 404s
    This prevents error reporting services like Rollbar from getting clogged
    up with 404 messages due to `ActionController::RoutingError`.

--- config/template.rb
+++ config/template.rb
@@ -32,5 +32,6 @@ apply "config/environments/production.rb"
 apply "config/environments/test.rb"
 template "config/environments/staging.rb.tt"
 
+route 'match "*unmatched", to: "errors#route_not_found", via: :all'
 route 'root "home#index"'
 route %Q(mount Sidekiq::Web => "/sidekiq" # monitoring console\n)
