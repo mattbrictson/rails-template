a9c16ceb65de7565276c9cc377cf1e705cf7eec0
Matt Brictson <matt@mattbrictson.com>
2018-12-26 09:34:14 -0600

    Add rack-canonical-host gem
    This provides an easy way to e.g. strip the "www." prefix when serving
    an app from a bare domain.

--- config.ru.rb
+++ config.ru.rb
@@ -9,3 +9,9 @@ end
     RUBY
   end
 end
+
+insert_into_file "config.ru", before: /^run Rails.application/ do
+  <<-RUBY
+use Rack::CanonicalHost, ENV["HOSTNAME"] if ENV["HOSTNAME"].present?
+  RUBY
+end
