60370e67bf7b572a4ae4c37eb8204395093e224c
Matt Brictson <matt@mattbrictson.com>
2019-10-23 10:10:14 -0700

    Improve Docker support by changing HOSTNAME var
    Docker containers set a HOSTNAME var by default which is not what our
    `config.ru` is expecting. To avoid this conflict, change the name of the
    variable we expect to RAILS_HOSTNAME. This allows a Docker container to
    run the Rails app out of the box.
    
    Fixes #21

--- config.ru.rb
+++ config.ru.rb
@@ -1,5 +1,5 @@
 insert_into_file "config.ru", before: /^run Rails.application/ do
   <<-RUBY
-use Rack::CanonicalHost, ENV["HOSTNAME"] if ENV["HOSTNAME"].present?
+use Rack::CanonicalHost, ENV["RAILS_HOSTNAME"] if ENV["RAILS_HOSTNAME"].present?
   RUBY
 end
