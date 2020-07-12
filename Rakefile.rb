8fc88022233432c256d85333cefa1a746b464092
Matt Brictson <matt@mattbrictson.com>
2020-07-11 20:08:15 -0700

    Use overcommit to run rake lint checks

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,11 +1,10 @@
 append_to_file "Rakefile" do
   <<~RUBY
 
-  begin
-    require "rubocop/rake_task"
-    RuboCop::RakeTask.new
-    task default: %w[test test:system rubocop]
-  rescue LoadError # rubocop:disable Lint/HandleExceptions
+  task :overcommit do
+    sh "bundle exec overcommit --run"
   end
+
+  task default: %w[test test:system overcommit]
   RUBY
 end
