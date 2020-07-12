1f63cce9bb447c2b3e7b0e4d533c317d20766cf6
Matt Brictson <matt@mattbrictson.com>
2020-07-12 16:30:57 -0700

    Don't use basic auth for sidekiq in local development

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,15 @@
 require "sidekiq/web"
 
-sidekiq_username = ENV["SIDEKIQ_WEB_USERNAME"]
-sidekiq_password = ENV["SIDEKIQ_WEB_PASSWORD"]
-
 Sidekiq::Web.app_url = "/"
-Sidekiq::Web.use(Rack::Auth::Basic, "Application") do |username, password|
-  if sidekiq_username.present? && sidekiq_password.present?
-    ActiveSupport::SecurityUtils.secure_compare(username, sidekiq_username) &
-      ActiveSupport::SecurityUtils.secure_compare(password, sidekiq_password)
+
+unless Rails.env.development?
+  sidekiq_username = ENV["SIDEKIQ_WEB_USERNAME"]
+  sidekiq_password = ENV["SIDEKIQ_WEB_PASSWORD"]
+
+  Sidekiq::Web.use(Rack::Auth::Basic, "Application") do |username, password|
+    if sidekiq_username.present? && sidekiq_password.present?
+      ActiveSupport::SecurityUtils.secure_compare(username, sidekiq_username) &
+        ActiveSupport::SecurityUtils.secure_compare(password, sidekiq_password)
+    end
   end
 end
