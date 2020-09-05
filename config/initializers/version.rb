489d7ab56b0bc8b2fac20f4c4df7dc468b4220fe
Matt Brictson <matt@mattbrictson.com>
2020-09-05 14:35:20 -0700

    Include more files in rubocop checks

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,11 +1,11 @@
-TomoReleaseData = begin
+TOMO_RELEASE_DATA = begin
   json_path = Rails.root.join(".tomo_release.json")
   data = json_path.file? ? JSON.parse(IO.read(json_path)) : {}
   data.freeze
 end
 
-Rails.application.config.version = TomoReleaseData.fetch("revision", "N/A")
+Rails.application.config.version = TOMO_RELEASE_DATA.fetch("revision", "N/A")
 Rails.application.config.version_time = begin
-  timestamp = TomoReleaseData.fetch("revision_date", Time.current.to_s)
+  timestamp = TOMO_RELEASE_DATA.fetch("revision_date", Time.current.to_s)
   Time.zone.parse(timestamp)
 end
