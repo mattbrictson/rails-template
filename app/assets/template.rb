f9143ae9fe8c5d0ed217fd6a23f32fff940efa83
Matt Brictson <matt@mattbrictson.com>
2021-12-28 16:29:11 -0600

    Only emit scss files if --css=sass

--- app/assets/template.rb
+++ app/assets/template.rb
@@ -1,5 +1,7 @@
-empty_directory_with_keep_file "app/assets/fonts"
-empty_directory "app/assets/stylesheets/mixins"
-copy_file "app/assets/stylesheets/colors.scss"
-copy_file "app/assets/stylesheets/application.sass.scss", force: true
-copy_file "app/assets/stylesheets/mixins/typography.scss"
+if options[:css] == "sass"
+  empty_directory_with_keep_file "app/assets/fonts"
+  empty_directory "app/assets/stylesheets/mixins"
+  copy_file "app/assets/stylesheets/colors.scss"
+  copy_file "app/assets/stylesheets/application.sass.scss", force: true
+  copy_file "app/assets/stylesheets/mixins/typography.scss"
+end
