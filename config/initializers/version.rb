8f7628fb98c46c1a78113c58832cc4e67ff031ab
Matt Brictson <matt@mattbrictson.com>
2022-01-22 15:35:41 -0800

    Support heroku dyno metadata

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,11 +1,11 @@
-TOMO_RELEASE_DATA = begin
-  json_path = Rails.root.join(".tomo_release.json")
-  data = json_path.file? ? JSON.parse(IO.read(json_path)) : {}
-  data.freeze
-end
+release_data = \
+  if (tomo_json = Rails.root.join(".tomo_release.json")).file?
+    JSON.parse(File.read(tomo_json))
+  elsif (heroku_commit = ENV["HEROKU_SLUG_COMMIT"]).present?
+    { "revision" => heroku_commit, "revision_date" => ENV["HEROKU_RELEASE_CREATED_AT"] }
+  else
+    { "revision" => "N/A", "revision_date" => Time.current.to_s }
+  end
 
-Rails.application.config.version = TOMO_RELEASE_DATA.fetch("revision", "N/A")
-Rails.application.config.version_time = begin
-  timestamp = TOMO_RELEASE_DATA.fetch("revision_date", Time.current.to_s)
-  Time.zone.parse(timestamp)
-end
+Rails.application.config.version = release_data["revision"]
+Rails.application.config.version_time = Time.zone.parse(release_data["revision_date"])
