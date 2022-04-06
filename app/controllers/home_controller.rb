39c7c56104537bde0c38ae2b6a92fd4e3a82060f
carl-printreleaf <cdileo@printreleaf.com>
2022-04-06 17:45:14 -0600

    Test commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def create
+    data = nil
+    @a = 1
+    [1,2,3].each {|n| n + 1}.each {|n| n + 1}.each {|n| n + 1}.each {|n| n + 1}.each {|n| n + 1}.each {|n| n + 1}
+
+
+
+
+  end
 end
