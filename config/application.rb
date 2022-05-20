a3f450f8f5c28cc8b1ec404e2c4858be5c033c68
Matt Brictson <matt@mattbrictson.com>
2022-05-20 16:53:01 -0700

    Ensure app/frontend is not in autoload paths

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,11 @@
 gsub_file "config/application.rb",
           "# config.time_zone = 'Central Time (US & Canada)'",
           'config.time_zone = "Pacific Time (US & Canada)"'
+
+if install_vite?
+  insert_into_file "config/application.rb", <<-RUBY, before: "  end"
+
+    # Prevents Rails from trying to eager-load the contents of app/frontend
+    config.javascript_path = "frontend"
+  RUBY
+end
