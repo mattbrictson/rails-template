a7b3831c095261112df62860ca76d6d06adc9e1c
Matt Brictson <matt@mattbrictson.com>
2023-05-24 08:42:46 -0700

    Fix rubocop issue in InlineSvgHelper

--- app/helpers/inline_svg_helper.rb
+++ app/helpers/inline_svg_helper.rb
@@ -4,6 +4,6 @@ module InlineSvgHelper
     svg = svg.sub(/\A<svg/, '<svg role="img"')
     svg = svg.sub(/\A<svg.*?>/, safe_join(['\0', "\n", tag.title(title)])) if title.present?
 
-    svg.strip.html_safe
+    svg.strip.html_safe # rubocop:disable Rails/OutputSafety
   end
 end
