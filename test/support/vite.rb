e4e09e72d2751daceb11d36e7887c9cecbfcbd0b
Matt Brictson <matt@mattbrictson.com>
2023-06-22 17:19:07 -0700

    More concise code style

--- test/support/vite.rb
+++ test/support/vite.rb
@@ -1,6 +1,5 @@
 return if ViteRuby.config.auto_build
 
 # Compile assets once at the start of testing
-Benchmark.ms { ViteRuby.instance.builder.build }.tap do |millis|
-  puts format("Built Vite assets (%.1fms)", millis)
-end
+millis = Benchmark.ms { ViteRuby.instance.builder.build }
+puts format("Built Vite assets (%.1fms)", millis)
