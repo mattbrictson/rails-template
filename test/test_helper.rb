ef0fd26c4b8cbe23da209d94d94de6d9ebdd5ed0
Matt Brictson <matt@mattbrictson.com>
2023-06-30 17:35:03 -0700

    Remove redundant './'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 # This has to come first
-require_relative "./support/rails"
+require_relative "support/rails"
 
 # Load everything else from test/support
 Dir[File.expand_path("support/**/*.rb", __dir__)].sort.each { |rb| require(rb) }
