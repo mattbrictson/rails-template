e22f0b9439abf24434725780e8880fcb9b1230f4
Matt Brictson <matt@mattbrictson.com>
2023-10-05 20:44:13 -0700

    Don't autoload puma/plugin in Rails 7.1

--- lib/template.rb
+++ lib/template.rb
@@ -1,3 +1,8 @@
+inject_into_file(
+  "config/application.rb",
+  "puma/plugin ",
+  after: /autoload_lib\(ignore: %w./
+)
 copy_file "lib/puma/plugin/open.rb"
 copy_file "lib/tasks/auto_annotate_models.rake"
 copy_file "lib/tasks/erblint.rake"
