c9e7abe701adc2e03f5824f7f4c7a7557d787c35
Brodie Nguyen <info@brodienguyen.com>
2023-10-28 14:36:35 +1100

    Adapt LayoutHelper to HAML context

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,13 +1,13 @@
 module LayoutHelper
   # Used to achieve nested layouts without content_for. This helper relies on
-  # Rails internals, so beware that it make break with future major versions
+  # Rails internals, so beware that it may break with future major versions
   # of Rails. Inspired by http://stackoverflow.com/a/18214036
   #
   # Usage: For example, suppose "child" layout extends "parent" layout.
-  # Use <%= yield %> as you would with non-nested layouts, as usual. Then on
-  # the very last line of layouts/child.html.erb, include this:
+  # Use `= yield` as you would with non-nested layouts, as usual. Then on
+  # the very last line of layouts/child.html.haml, include this:
   #
-  #     <% parent_layout "parent" %>
+  #     - parent_layout "parent"
   #
   def parent_layout(layout)
     @view_flow.set(:layout, output_buffer) # rubocop:disable Rails/HelperInstanceVariable
