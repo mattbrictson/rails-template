6860b8b50d3c60bb8f95dc991a598bed67fa2326
Brodie Nguyen <info@brodienguyen.com>
2023-10-28 14:18:11 +1100

    Update the Rakefile to reflect the use of haml_lint

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,12 +4,12 @@ append_to_file "Rakefile" do
   Rake::Task[:default].prerequisites.clear if Rake::Task.task_defined?(:default)
 
   desc "Run all checks"
-  task default: %w[test:all rubocop erblint yarn:lint] do
+  task default: %w[test:all rubocop haml_lint yarn:lint] do
     Thor::Base.shell.new.say_status :OK, "All checks passed!"
   end
 
   desc "Apply auto-corrections"
-  task fix: %w[rubocop:autocorrect_all erblint:autocorrect yarn:fix] do
+  task fix: %w[rubocop:autocorrect_all haml_lint:autocorrect yarn:fix] do
     Thor::Base.shell.new.say_status :OK, "All fixes applied!"
   end
   RUBY
